512539d6f77cbc7e625dd02d5bfef6c4ab2109cd
JulitaZochowska <julita.zochowska@outlook.com>
2023-07-15 08:37:31 +0200

    step 3 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,13 @@ import React, { Component } from 'react';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -43,33 +49,3 @@ export class App extends Component {
 }
 
 export default App;
-
-// export const App = () => {
-//   const state = {
-//     contacts: [],
-//     name: '',
-//   };
-
-//   const addContact = newContact => {
-//     console.log(newContact);
-//     this.setState(prevState => ({
-//       contacts: [...prevState.contacts, newContact],
-//     }));
-//   };
-
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101',
-//       }}
-//     >
-//       <ContactForm addContact={addContact} />
-//       <ContactList contacts={state.contacts} />
-//     </div>
-//   );
-// };
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 
 class ContactList extends Component {
+  state = {
+    filter: '',
+  };
+
+  filterContacts = contacts =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   render() {
     return (
       <div>
         <h3>Contacts</h3>
+        <label>Find contacts by name</label>
+        <input
+          type="text"
+          name="filter"
+          value={this.state.filter}
+          onChange={this.handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
         <ul>
-          {this.props.contacts.map(contact => (
+          {/* filtruje wszystko co przychodzi z API, to co przychodzi z API jest w nawiasie */}
+          {/* (this.props.contact)to to, co przychodzi z API */}
+          {this.filterContacts(this.props.contacts).map(contact => (
             <li key={contact.id}>
               {contact.name} {contact.number}
             </li>
